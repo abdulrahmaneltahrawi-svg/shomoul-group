10c52c6a18b8a42a48b9f7f34cd543603a3819e8
Abdulrahman <abdulrhmanelthrawi@gmail.com>
2026-07-07 11:17:30 +0300

    github domin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), tailwindcss()],
+    // إذا كنت في وضع الإنتاج (Build) استخدم المسار، وإذا كنت في وضع التطوير استخدم الجذر
+    base: mode === 'production' ? '/shomoul-group/' : '/',
+  }
 })
